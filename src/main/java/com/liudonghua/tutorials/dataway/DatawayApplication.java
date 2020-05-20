e25b7a49a20c4c6fc719a1c15a6c8904a5936026
liudonghua123 <liudonghua123@gmail.com>
2020-05-20 22:46:03 +0800

    add intial dataway demo code

--- src/main/java/com/liudonghua/tutorials/dataway/DatawayApplication.java
+++ src/main/java/com/liudonghua/tutorials/dataway/DatawayApplication.java
@@ -2,7 +2,11 @@ package com.liudonghua.tutorials.dataway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import net.hasor.spring.boot.EnableHasor;
+import net.hasor.spring.boot.EnableHasorWeb;
 
+@EnableHasor
+@EnableHasorWeb
 @SpringBootApplication
 public class DatawayApplication {
 
